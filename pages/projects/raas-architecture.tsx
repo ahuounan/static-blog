f2307598f627e04c44715dc37cc9087110ed248e
Alan Hu <on.alanhu@gmail.com>
2021-07-06 22:52:51 +0100

    fix: set article page to under development

--- pages/projects/raas-architecture.tsx
+++ pages/projects/raas-architecture.tsx
@@ -1,25 +1,19 @@
 import { PageLayout } from 'components/layouts/PageLayout';
 import { ArticleContent } from 'components/contents/ArticleContent';
-import logo from 'public/pikachu.png';
 
 export default function RaasArchitecture() {
   return (
     <PageLayout
       title="Risk as a Service - Architecture"
       header={
-        <>
-          <h2>Risk as a Service</h2>
-          <h1>Architecture</h1>
-        </>
+        <div>
+          <h2 className="text-xl">Risk as a Service</h2>
+          <h1 className="text-3xl">Architecture</h1>
+        </div>
       }>
       <ArticleContent
         paragraphs={[
-          { content: <>test</> },
-          {
-            img: logo,
-            caption: 'test',
-            alt: 'test'
-          }
+          { content: <>Content under development, please check back later!</> }
         ]}
       />
     </PageLayout>
