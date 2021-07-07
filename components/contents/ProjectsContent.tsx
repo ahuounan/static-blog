4cc2b5a7488eee52b220dda70c9dc293a10d6d2f
Alan Hu <on.alanhu@gmail.com>
2021-07-07 18:07:17 +0100

    fix: html routing issue

--- components/contents/ProjectsContent.tsx
+++ components/contents/ProjectsContent.tsx
@@ -8,19 +8,19 @@ export function ProjectsContent() {
         <ProjectItem
           title="Strange Attractor Simulator"
           description="A small project playing around with Canvas."
-          link="/projects/raas-architecture"
+          link="/projects/raas-architecture.html"
         />
         <ProjectItem
           title="CantoChat"
           subtitle="Writing a Dependency Manager"
           description="C# is a very class-based language and different from Javascript. Here, I write a simple Dependency Manager class to help manage the different game services."
-          link="/projects/raas-architecture"
+          link="/projects/raas-architecture.html"
         />
         <ProjectItem
           title="Personal Blog Site"
           subtitle="Keeping Things Simple"
           description="After having worked on a mobile and web app, writing a personal blog site couldn't take me more than a few days. Or so I thought. Join me in my journey to learning (again) that Occam was right, and that the simplest way is the best way."
-          link="/projects/raas-architecture"
+          link="/projects/raas-architecture.html"
         />
       </Section>
       <Section title="JP Morgan">
@@ -28,24 +28,24 @@ export function ProjectsContent() {
           title="Risk as a Service"
           subtitle="Architecture"
           description="After learning React/Redux at Endeavor, I thought I had a good grasp of front-end architecture. Well, the world is a big place. It was a challenging but valuable experience not only to learn rxjs but also to unlearn Redux."
-          link="/projects/raas-architecture"
+          link="/projects/raas-architecture.html"
         />
         <ProjectItem
           title="Risk as a Service"
           subtitle="Building a Charting Toolbox"
-          link="/projects/raas-architecture"
+          link="/projects/raas-architecture.html"
         />
       </Section>
       <Section title="Endeavor Streaming">
         <ProjectItem
           title="Dice Mobile"
           subtitle="Architecture"
-          link="/projects/raas-architecture"
+          link="/projects/raas-architecture.html"
         />
         <ProjectItem
           title="Dice Mobile"
           subtitle="Refactoring the Chromecast State Machine"
-          link="/projects/raas-architecture"
+          link="/projects/raas-architecture.html"
         />
       </Section>
     </>
